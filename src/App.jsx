cbb01100b099700e30e92b8652cb74cc7d56326b
nazib ullah <najibullahnajib758@gmail.com>
2024-01-07 13:21:09 +0600

    nn

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Product from './pages/product/Product'
-import Products from './pages/products/Products'
-import Not_found from './pages/Not_found'
-import Navbar from './components/navbar/Navbar'
-import Footer from './components/footer/Footer'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/product/Product";
+import Products from "./pages/products/Products";
+import Not_found from "./pages/Not_found";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className='app'>
+    <div className="app">
       <BrowserRouter>
-      <Navbar/>
-      <Routes>
-       <Route path='/' element={<Home/>}/>
-       <Route path='/product/:id' element={<Product/>}/>
-       <Route path='/products/:id' element={<Products/>}/>
-       <Route path='*' element={<Not_found/>}/>
-
-      </Routes>
-      <Footer/>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/e-commerce" element={<Home />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/products/:id" element={<Products />} />
+         
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
